7f07f9b1400e61da57ef709d4ad01bfba511a307
YatharthZinzuwadia <yatharthzinzuwadia@gmail.com>
2024-03-29 14:56:35 +0530

    deployed on netlify

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -11,7 +11,7 @@ const DirectoryItem = ({ category }) => {
 
   const onNavigateHandler = () => navigate(route);
   return (
-    <DirectoryItemContainer onClick={onNavigateHandlergit }>
+    <DirectoryItemContainer onClick={onNavigateHandler }>
       <BackgroundImage imageUrl={imageUrl} />
       <Body>
         <h2>{title}</h2>
